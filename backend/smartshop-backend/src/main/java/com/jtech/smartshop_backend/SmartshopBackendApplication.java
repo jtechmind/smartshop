cdc7104edc43e363e53db5eff5505058306cf848
jtechmind <jtechmind@gmail.com>
2026-05-20 16:41:52 +0530

    feat: initialize Spring Boot backend project

---
+++ backend/smartshop-backend/src/main/java/com/jtech/smartshop_backend/SmartshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jtech.smartshop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartshopBackendApplication.class, args);
+	}
+
+}
---
+++ backend/smartshop-backend/src/test/java/com/jtech/smartshop_backend/SmartshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtech.smartshop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
